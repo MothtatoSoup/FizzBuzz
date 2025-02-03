f8d67b6493c6e1220e9a339d7a49c89d89f3c275
MothtatoSoup <maddy.puryear@outlook.com>
2025-02-03 12:45:27 -0500

    set up files

---
+++ FizzBuzz.js
@@ -0,0 +1,23 @@
+// Define Constants for the form and the result display area
+
+// Add the first line of the event listener to handle form submission
+
+        // Prevent the form from refershing the page
+        // event.preventDefault();
+
+// Get the number from the form input
+
+//FizzBuzz code
+for (let i = 1; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+          console.log("FizzBuzz");
+     } else if(i % 3 == 0){
+          console.log("Fizz");
+     } else if(i % 5 == 0){
+          console.log("Buzz");
+     } else{
+          console.log(i);
+     }
+}
+
+//Result.textContent = output;
