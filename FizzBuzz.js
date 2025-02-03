a634a609af1102ad212f7905c515802dfca94412
MothtatoSoup <maddy.puryear@outlook.com>
2025-02-03 13:00:07 -0500

    made variables

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,14 +1,20 @@
 // Define Constants for the form and the result display area
 
+const userInput = document.getElementById("Input")
+const userResults = document.getElementById("results")
+
 // Add the first line of the event listener to handle form submission
 
+
+
+
         // Prevent the form from refershing the page
-        // event.preventDefault();
+        event.preventDefault();
 
 // Get the number from the form input
 
 //FizzBuzz code
-for (let i = 1; i <= 100; i++){
+For (let i = 1; i <= 100; i++){
     if(i % 3 == 0 && i % 5 == 0){
           console.log("FizzBuzz");
      } else if(i % 3 == 0){
