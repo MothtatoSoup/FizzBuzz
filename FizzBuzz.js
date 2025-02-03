4be921c617c5f6880789e129c966a57d0d3641d5
MothtatoSoup <maddy.puryear@outlook.com>
2025-02-03 13:22:43 -0500

    did a bunch of stuff

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,29 +1,31 @@
 // Define Constants for the form and the result display area
 
-const userInput = document.getElementById("Input")
-const userResults = document.getElementById("results")
-
+const userInput = document.getElementById("Input");
+const userResults = document.getElementById("result");
+console.log("Defined Variables")
+let output;
 // Add the first line of the event listener to handle form submission
-
-
-
-
-        // Prevent the form from refershing the page
-        event.preventDefault();
-
-// Get the number from the form input
-
-//FizzBuzz code
-For (let i = 1; i <= 100; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-          console.log("FizzBuzz");
-     } else if(i % 3 == 0){
-          console.log("Fizz");
-     } else if(i % 5 == 0){
-          console.log("Buzz");
+document.getElementById("Button").addEventListener("onClick", function (x) {
+     Event.preventDefault();  // Prevent the form from refershing the page
+     console.log("Event works")
+     // Get the number from the form input
+     x = userInput.value();
+     console.log("Defined let vars")
+     //FizzBuzz code
+     if(x % 3 == 0 && x % 5 == 0){
+           console.log("FizzBuzz");
+           output = "FizzBuzz";
+     } else if(x % 3 == 0){
+           console.log("Fizz");
+           output = "Fizz";
+     } else if(x % 5 == 0){
+           console.log("Buzz");
+           output = "Buzz";
      } else{
-          console.log(i);
-     }
-}
-
-//Result.textContent = output;
+           console.log(x);
+           output = "x";
+      }
+          
+})
+console.log("End of loop")
+userResults.textContent = output;
